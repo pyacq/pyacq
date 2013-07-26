87aaab9f524d4f337f9025f8ada16c5f0220c705
Samuel Garcia <sam.garcia.die@gmail.com>
2013-07-26 15:03:06 +0200

    Digital stream with measurement computing.

--- example/measurement_computing/mc_oscilloscope.py
+++ example/measurement_computing/mc_oscilloscope.py
@@ -7,8 +7,6 @@ from pyacq import StreamHandler, MeasurementComputingMultiSignals
 from pyacq.gui import Oscilloscope, TimeFreq
 
 import msgpack
-#~ import gevent
-#~ import zmq.green as zmq
 
 from PyQt4 import QtCore,QtGui
 
@@ -24,10 +22,12 @@ def test1():
     dev.configure( board_num = 0,
                           sampling_rate =10000.,
                           #~ sampling_rate =1000.,
-                          buffer_length = 51.2,
-                          channel_indexes = range(64),
+                          buffer_length = 60.,
+                          #~ channel_indexes = range(64),
                           #~ channel_indexes = range(32),
-                          #~ channel_indexes = [0,]
+                          channel_indexes = [0,12,25,56],
+                          #~ digital_port = [0, 1, 2],
+                          digital_port = [],
                                 )
     dev.initialize()
     dev.start()
@@ -38,9 +38,9 @@ def test1():
     w1.change_param_global(xsize = 5., refresh_interval = 100, mode = 'scan', ylims = [-8., 8.])
     w1.show()
     
-    #~ w2 = TimeFreq(stream = dev.streams[0], max_visible_on_open = 4)
-    #~ w2.change_param_global(refresh_interval = 100, xsize = 2.)
-    #~ w2.show()
+    w2 = TimeFreq(stream = dev.streams[0], max_visible_on_open = 4)
+    w2.change_param_global(refresh_interval = 100, xsize = 2.)
+    w2.show()
     
     app.exec_()
     
