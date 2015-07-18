51261bfa31171a9f2994f6da65eef088ac9af1f3
Luke Campagnola <luke.campagnola@gmail.com>
2015-07-18 16:30:11 +0200

    Add client socket-sharing test

--- pyacq/core/tests/test_rpc.py
+++ pyacq/core/tests/test_rpc.py
@@ -66,8 +66,19 @@ def test_rpc():
     assert a.result() == 3
     assert c.result() == 11
 
-
+    # test multiple clients sharing one socket
+    server2 = Server1(name='some_server2', addr='tcp://*:5153')
+    serve_thread2 = threading.Thread(target=server2.run_forever, daemon=True)
+    serve_thread2.start()
     
+    client3 = RPCClient('some_server2', 'tcp://localhost:5153',
+                        socket=client2._socket)
+    
+    a = client2.add(1, 2)
+    b = client3.add(3, 4)
+    assert b.result() == 7
+    assert a.result() == 3
+
 
 if __name__ == '__main__':
     test_rpc()
