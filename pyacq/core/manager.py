15d32148cbdec0cb54aee1990147d28a608938e8
Luke Campagnola <luke.campagnola@gmail.com>
2015-07-20 21:11:05 +0200

    Manager unit tests pass

--- pyacq/core/manager.py
+++ pyacq/core/manager.py
@@ -18,7 +18,7 @@ class Manager(RPCServer):
 
     def connect_host(self, name, addr):
         if name not in self.hosts:
-            hp = _Host(self, name, addr)
+            hp = _Host(name, addr)
             self.hosts[name] = hp
 
     def disconnect_host(self, name):
@@ -27,13 +27,13 @@ class Manager(RPCServer):
     def list_hosts(self):
         return list(self.hosts.keys())
     
-    def add_nodegroup(self, host, name):
+    def create_nodegroup(self, host, name):
         if name in self.nodegroups:
             raise KeyError("Nodegroup named %s already exists" % name)
         host = self.hosts[host]
         addr = 'tcp://%s:*' % (host.rpc_hostname)
         _, addr = host.client.new_nodegroup(name, addr)
-        ng = _NodeGroup(self, name, addr)
+        ng = _NodeGroup(host, name, addr)
         host.add_nodegroup(ng)
         self.nodegroups[name] = ng
 
@@ -43,6 +43,15 @@ class Manager(RPCServer):
         else:
             return self.hosts[host].list_nodegroups()
 
+    def create_node(self, nodegroup, name, classname, **kwargs):
+        if name in self.nodes:
+            raise KeyError("Node named %s already exists" % name)
+        ng = self.nodegroups[nodegroup]
+        ng.client.create_node(name, classname, **kwargs)
+        node = _Node(ng, name, classname)
+        self.nodes[name] = node
+        ng.add_node(name, node)
+
     def list_nodes(self, nodegroup=None):
         if nodegroup is None:
             return list(self.nodes.keys())
@@ -51,8 +60,7 @@ class Manager(RPCServer):
 
         
 class _Host(object):
-    def __init__(self, mgr, name, addr):
-        self.mgr = mgr
+    def __init__(self, name, addr):
         self.rpc_address = addr
         self.rpc_name = name
         self.client = RPCClient(name, addr)
@@ -67,15 +75,23 @@ class _Host(object):
 
 
 class _NodeGroup(object):
-    def __init__(self, mgr, name, addr):
-        self.mgr = mgr
+    def __init__(self, host, name, addr):
+        self.host = host
         self.rpc_address = addr
         self.rpc_name = name
         self.client = RPCClient(name, addr)
         self.nodes = {}
 
+    def add_node(self, name, node):
+        self.nodes[name] = node
+
     def list_nodes(self):
         return list(self.nodes.keys())
     
         
+class _Node(object):
+    def __init__(self, nodegroup, name, classname):
+        self.nodegroup = nodegroup
+        self.name = name
+        self.classname = classname
         
