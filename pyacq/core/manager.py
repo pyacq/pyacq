7aa05c71f9828421aed71646f215afad7b82454c
Luke Campagnola <luke.campagnola@gmail.com>
2015-07-20 21:01:49 +0200

    manager closer to functional

--- pyacq/core/manager.py
+++ pyacq/core/manager.py
@@ -18,43 +18,64 @@ class Manager(RPCServer):
 
     def connect_host(self, name, addr):
         if name not in self.hosts:
-            hp = HostProxy(self, name, addr)
+            hp = _Host(self, name, addr)
             self.hosts[name] = hp
-        return self.hosts[name]
 
     def disconnect_host(self, name):
         self.hosts[name].close()
 
+    def list_hosts(self):
+        return list(self.hosts.keys())
+    
     def add_nodegroup(self, host, name):
+        if name in self.nodegroups:
+            raise KeyError("Nodegroup named %s already exists" % name)
         host = self.hosts[host]
         addr = 'tcp://%s:*' % (host.rpc_hostname)
-        host.client.new_nodegroup(name, addr)
-        ng = NodeGroupProxy(self, name, addr)
+        _, addr = host.client.new_nodegroup(name, addr)
+        ng = _NodeGroup(self, name, addr)
+        host.add_nodegroup(ng)
         self.nodegroups[name] = ng
 
+    def list_nodegroups(self, host=None):
+        if host is None:
+            return list(self.nodegroups.keys())
+        else:
+            return self.hosts[host].list_nodegroups()
+
+    def list_nodes(self, nodegroup=None):
+        if nodegroup is None:
+            return list(self.nodes.keys())
+        else:
+            return self.nodegroups[nodegroup].list_nodes()
+
         
-class HostProxy(object):
+class _Host(object):
     def __init__(self, mgr, name, addr):
         self.mgr = mgr
         self.rpc_address = addr
         self.rpc_name = name
         self.client = RPCClient(name, addr)
         self.nodegroups = {}
-        self.rpc_hostname = name.partition('//')[2].rpartition(':')[0]
+        self.rpc_hostname = addr.partition('//')[2].rpartition(':')[0]
 
-    def add_nodegroup(self, name):
-        ng = self.mgr.add_nodegroup(self.rpc_name, name)
-        self.nodegroups[name] = ng
-        return ng
+    def add_nodegroup(self, ng):
+        self.nodegroups[ng.rpc_name] = ng
+    
+    def list_nodegroups(self):
+        return list(self.nodegroups.keys())
 
 
-class NodeGroupProxy(object):
+class _NodeGroup(object):
     def __init__(self, mgr, name, addr):
         self.mgr = mgr
         self.rpc_address = addr
         self.rpc_name = name
         self.client = RPCClient(name, addr)
+        self.nodes = {}
 
+    def list_nodes(self):
+        return list(self.nodes.keys())
     
         
         
