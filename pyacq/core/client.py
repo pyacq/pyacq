4620d715743df47fdb54de4cae638ea71e8828be
Luke Campagnola <luke.campagnola@gmail.com>
2015-07-21 17:46:31 +0200

    client example runs

---
+++ pyacq/core/client.py
@@ -0,0 +1,66 @@
+from .rpc import RPCClient
+
+
+class ManagerProxy(RPCClient):
+    def __init__(self, proc):
+        self.proc = proc
+        RPCClient.__init__(self, proc.name, proc.addr)
+        self._host_proxy = {}
+
+    def connect_host(self, name, addr):
+        self._call_method('connect_host', name, addr)
+        if name not in self._host_proxy:
+            self._host_proxy[name] = HostProxy(name, addr)
+        return self._host_proxy[name]
+    
+    def default_host(self):
+        name, addr = self._call_method('default_host')
+        if name not in self._host_proxy:
+            self._host_proxy[name] = HostProxy(self, name, addr)
+        return self._host_proxy[name]
+
+    def create_nodegroup(self, host=None, name=None):
+        if host is None:
+            host = self.default_host()
+        return host.create_nodegroup(name)
+
+
+class HostProxy(RPCClient):
+    def __init__(self, mgr, name, addr):
+        self.mgr = mgr
+        self.name = name
+        self.addr = addr
+        RPCClient.__init__(self, name, addr)
+        self._nodegroup_proxy = {}
+        
+    def create_nodegroup(self, name=None):
+        if name is None:
+            name = self.mgr.suggest_nodegroup_name()
+        _, addr = self.mgr._call_method('create_nodegroup', self.name, name)
+        self._nodegroup_proxy[name] = NodeGroupProxy(self, name, addr)
+        return self._nodegroup_proxy[name]
+
+
+class NodeGroupProxy(RPCClient):
+    def __init__(self, host, name, addr):
+        self.mgr = host.mgr
+        self.host = host
+        self.name = name
+        self.addr = addr
+        RPCClient.__init__(self, name, addr)
+        self._node_proxy = {}
+        
+    def create_node(self, classname, name=None, **kwargs):
+        if name is None:
+            name = self.mgr.suggest_node_name()
+        self.mgr.create_node(self.name, name, classname, **kwargs)
+        self._node_proxy[name] = NodeProxy(self, name)
+        
+        
+class NodeProxy(object):
+    def __init__(self, nodegroup, name):
+        self.nodegroup = nodegroup
+        self.name = name
+        
+    def __getattr__(self, name):
+        return lambda *args, **kwargs: getattr(self.nodegroup, name)(*args, **kwargs)
--- pyacq/core/manager.py
+++ pyacq/core/manager.py
@@ -1,5 +1,16 @@
 from .rpc import RPCServer, RPCClientSocket, RPCClient
+from .client import ManagerProxy
+from .processspawner import ProcessSpawner
+from .host import Host
 
+
+def create_manager(mode='rpc'):
+    if mode == 'local':
+        return Manager(name='manager', addr='tcp://*:*')
+    else:
+        proc = ProcessSpawner(Manager, name='manager', addr='tcp://127.0.0.1:*')
+        return ManagerProxy(proc)
+        
 
 class Manager(RPCServer):
     """Manager is a central point of control for connecting to hosts, creating
@@ -19,11 +30,60 @@ class Manager(RPCServer):
     addr : str
         The address for the manager's RPC server.
     """
+    
+    # Classes used internally for bookkeeping
+    class _Host(object):
+        def __init__(self, name, addr):
+            self.rpc_address = addr
+            self.rpc_name = name
+            self.client = RPCClient(name, addr)
+            self.nodegroups = {}
+            self.rpc_hostname = addr.partition('//')[2].rpartition(':')[0]
+
+        def add_nodegroup(self, ng):
+            self.nodegroups[ng.rpc_name] = ng
+        
+        def list_nodegroups(self):
+            return list(self.nodegroups.keys())
+
+
+    class _NodeGroup(object):
+        def __init__(self, host, name, addr):
+            self.host = host
+            self.rpc_address = addr
+            self.rpc_name = name
+            self.client = RPCClient(name, addr)
+            self.nodes = {}
+
+        def add_node(self, name, node):
+            self.nodes[name] = node
+
+        def list_nodes(self):
+            return list(self.nodes.keys())
+
+        def delete_node(self, name):
+            del self.nodes[name]
+        
+            
+    class _Node(object):
+        def __init__(self, nodegroup, name, classname):
+            self.nodegroup = nodegroup
+            self.name = name
+            self.classname = classname
+    
+    
     def __init__(self, name, addr):
         RPCServer.__init__(self, name, addr)
         self.hosts = {}  # name:HostProxy
         self.nodegroups = {}  # name:NodegroupProxy
         self.nodes = {}  # name:NodeProxy
+        
+        # auto-generated host on the local machine
+        self._default_host = None
+        
+        # for auto-generated node / nodegroup names
+        self._next_nodegroup_name = 0
+        self._next_node_name = 0
         
         # shared socket for all RPC client connections
         self._rpc_socket = RPCClientSocket()
@@ -35,13 +95,24 @@ class Manager(RPCServer):
         Nodegroups can be spawned or closed.
         """
         if name not in self.hosts:
-            hp = _Host(name, addr)
+            hp = Manager._Host(name, addr)
             self.hosts[name] = hp
 
     def disconnect_host(self, name):
         """Disconnect the Manager from the Host identified by *name*.
         """
         self.hosts[name].close()
+        
+    def default_host(self):
+        """Return the RPC name and address of a default Host created by the
+        Manager.
+        """
+        if self._default_host is None:
+            addr = self._addr.rpartition(b':')[0] + b':*'
+            proc = ProcessSpawner(Host, name='default-host', addr=addr)
+            self._default_host = proc
+            self.connect_host(proc.name, proc.addr)
+        return self._default_host.name, self._default_host.addr
 
     def list_hosts(self):
         """Return a list of the identifiers for Hosts that the Manager is
@@ -65,9 +136,10 @@ class Manager(RPCServer):
         host = self.hosts[host]
         addr = 'tcp://%s:*' % (host.rpc_hostname)
         _, addr = host.client.new_nodegroup(name, addr)
-        ng = _NodeGroup(host, name, addr)
+        ng = Manager._NodeGroup(host, name, addr)
         host.add_nodegroup(ng)
         self.nodegroups[name] = ng
+        return name, addr
 
     def list_nodegroups(self, host=None):
         if host is None:
@@ -80,7 +152,7 @@ class Manager(RPCServer):
             raise KeyError("Node named %s already exists" % name)
         ng = self.nodegroups[nodegroup]
         ng.client.create_node(name, classname, **kwargs)
-        node = _Node(ng, name, classname)
+        node = Manager._Node(ng, name, classname)
         self.nodes[name] = node
         ng.add_node(name, node)
 
@@ -100,43 +172,14 @@ class Manager(RPCServer):
         del self.nodes[name]
         ng.delete_node(name)
 
-        
-class _Host(object):
-    def __init__(self, name, addr):
-        self.rpc_address = addr
-        self.rpc_name = name
-        self.client = RPCClient(name, addr)
-        self.nodegroups = {}
-        self.rpc_hostname = addr.partition('//')[2].rpartition(':')[0]
-
-    def add_nodegroup(self, ng):
-        self.nodegroups[ng.rpc_name] = ng
+    def suggest_nodegroup_name(self):
+        name = 'nodegroup-%d' % self._next_nodegroup_name
+        self._next_nodegroup_name += 1
+        return name
     
-    def list_nodegroups(self):
-        return list(self.nodegroups.keys())
-
-
-class _NodeGroup(object):
-    def __init__(self, host, name, addr):
-        self.host = host
-        self.rpc_address = addr
-        self.rpc_name = name
-        self.client = RPCClient(name, addr)
-        self.nodes = {}
-
-    def add_node(self, name, node):
-        self.nodes[name] = node
-
-    def list_nodes(self):
-        return list(self.nodes.keys())
-
-    def delete_node(self, name):
-        del self.nodes[name]
+    def suggest_node_name(self):
+        name = 'node-%d' % self._next_node_name
+        self._next_node_name += 1
+        return name
     
-        
-class _Node(object):
-    def __init__(self, nodegroup, name, classname):
-        self.nodegroup = nodegroup
-        self.name = name
-        self.classname = classname
-        
+    
