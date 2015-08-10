c18bb6685cfb67ebdc7480b0c925c16f7d2204c6
Luke Campagnola <luke.campagnola@gmail.com>
2015-08-09 21:47:14 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import pyacq
 setup(
     name = "pyacq",
     version = pyacq.__version__,
-    packages = ['pyacq', ],
+    packages = ['pyacq', 'pyacq.core', 'pyacq.viewers', 'pyacq.devices'],
     install_requires=[
                     'numpy',
                     'pyzmq',
