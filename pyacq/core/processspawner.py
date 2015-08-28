d36fbb6fc2b7a6e88f5fbce2f1d42cd3cad1c017
Samuel Garcia <sam/garcia.die@gmail.com>
2015-08-28 12:58:16 +0200

    Try smother end for processes. To avoid brutal kill of manager. create_manager also atexit.register(man.close)/

--- pyacq/core/processspawner.py
+++ pyacq/core/processspawner.py
@@ -81,6 +81,10 @@ class ProcessSpawner:
         self.proc.wait()
 
     def kill(self):
+        try:
+            self.stop()
+        except:
+            pass
         if self.proc.poll() is not None:
             return
         logging.info("Kill process: %d", self.proc.pid)
@@ -91,6 +95,6 @@ class ProcessSpawner:
         if self.proc.poll() is not None:
             return
         logging.info("Close process: %d", self.proc.pid)
-        self.client.close()
+        self.client.close(_timeout = 1.)
         self.proc.wait()
 
