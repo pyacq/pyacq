618ccfe6a34d304966725400bde5b025368f3b63
Samuel Garcia <sam/garcia.die@gmail.com>
2015-10-27 18:34:32 +0100

    in prgoress

--- pyacq/dsp/triggeraccumulator.py
+++ pyacq/dsp/triggeraccumulator.py
@@ -2,9 +2,23 @@ from pyqtgraph.Qt import QtCore
 import pyqtgraph as pg
 import numpy as np
 
-from ..core import (Node, register_node_type, ThreadPollInput, StreamConverter)
+
+from ..core import (Node, register_node_type, ThreadPollOuput)
 
 
+class ThreadPollOuputUntilPosLimit(ThreadPollOuput):
+    """
+    Thread waiting a futur pos in a stream.
+    """
+    limit_reached = QtCore.pyqtSignal()
+    def __init__(self, pos, pos_limit, **kargs):
+        ThreadPollOuput.__init__(self, **kargs)
+        self.pos = pos
+        self.pos_limit = pos_limit
+    
+    def process_data(self, pos, data):
+        if pos>self.pos_limit:
+            self.limit_reached.emit()
 
 
 class TriggerAccumulator(Node,  QtCore.QObject):
@@ -33,12 +47,16 @@ class TriggerAccumulator(Node,  QtCore.QObject):
 
     def after_input_connect(self, inputname):
         self.nb_channel, _ = self.inputs['signal'].params['shape']
-        
-
+        self.sample_rate = self.inputs['signal'].params['sample_rate']
+    
+    
     def _initialize(self):
-        self.thread = self._TriggerThread(self.input, self.output)
-        self.thread.change_params(self.params)
-        self.new_params.connect(self.thread.change_params)
+        self.trig_poller  = ThreadPollOuput(self.input['events'])
+        self.trig_poller.new_data.connect(self.on_new_trig)
+        
+        self.wait_list = []
+        self.recreate_stack()
+        
         
     
     def _start(self):
@@ -51,4 +69,40 @@ class TriggerAccumulator(Node,  QtCore.QObject):
     
     def on_params_change(self):
         pass
+
+    def on_new_trig(self, pos, data):
+        thread = ThreadPollOuputUntilPosLimit(pos+int(self.sample_rate*self.params['right_sweep'], parent = self)
+        thread.limit_reached.connect(self.on_limit_reached)
+        self.wait_list.append(thread)
+        thread.start()
+    
+    def on_limit_reached(self):
+        thread = self.sender()
+        pos = thread.pos
+        thread.stop()
+        thread.wait()
+        self.wait_list.pop(thread)
+
+    def recreate_stack(self):
+        n = self.stream['nb_channel']
+        stack_size = self.allParams['stack_size']
+        left_sweep = self.allParams['left_sweep']
+        right_sweep = self.allParams['right_sweep']
+        sr = self.stream['sampling_rate']
+
+        self.limit1 = l1 = int(left_sweep*sr)
+        self.limit2 = l2 = int(right_sweep*sr)
+        
+        self.t_vect = np.arange(l2-l1)/sr+left_sweep
+        self.stack = np.zeros((stack_size, n, l2-l1), dtype = self.stream.shared_array.dtype)
+        self.stack_pos = 0
+        
+        self.total_trig = 0
+
+    def reset_stack(self):
+        self.stack[:] = 0
+        self.stack_pos = 0
+        
+        self.total_trig = 0
+
 
