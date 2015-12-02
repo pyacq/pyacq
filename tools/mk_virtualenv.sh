211e86bdc5381412eafae697c3b7eefd7dcce4a6
Luke Campagnola <luke.campagnola@gmail.com>
2015-12-01 22:09:55 -0800

    add blosc to mk_virtualenv

--- tools/mk_virtualenv.sh
+++ tools/mk_virtualenv.sh
@@ -1,7 +1,7 @@
 # set up new virtualenv with all ppip-installable packages
 virtualenv --python=python3.4 venv
 . venv/bin/activate
-pip install pyzmq pytest numpy scipy pyqtgraph vispy colorama msgpack-python pyaudio
+pip install pyzmq pytest numpy scipy pyqtgraph vispy colorama msgpack-python pyaudio blosc
 
 # copy sip and pyqt from system packages (these are not pip installable)
 cp -r /usr/lib/python3/dist-packages/sip* venv/lib/python3.4/site-packages/
