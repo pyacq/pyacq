8de57cae8cac77e9f7c00f3a73c6546e844b3994
Luke Campagnola <luke.campagnola@gmail.com>
2016-01-24 22:30:48 -0800

    minor edits

--- example/video_display.py
+++ example/video_display.py
@@ -1,17 +1,17 @@
+"""
+Generate a random video feed and display it in an ImageViewer node.
+"""
 import numpy as np
 from pyacq import create_manager, ImageViewer, FakeVideoSource
 from pyqtgraph.Qt import QtCore, QtGui
 
-
-
-#view is a Node in local QApp
 app = QtGui.QApplication([])
 
 data = np.random.normal(size=(10, 100, 100))
 
 source = FakeVideoSource()
 
-viewer = ImageViewer(gfxlib='vispy')
+viewer = ImageViewer()
 viewer.configure()
 viewer.input.connect(source.output)
 viewer.initialize()
