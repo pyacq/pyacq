ffacfe702535d263505312774ed6c8dd53fe0aca
Samuel Garcia <sam/garcia.die@gmail.com>
2016-03-22 16:38:34 +0100

    Benchmark 2 opencl approach for SosFilter

---
+++ pyacq/dsp/tests/benchmark_opencl.py
@@ -0,0 +1,79 @@
+"""
+Some benchmark to compare CPU with numpy solution and home made OpenCL implementation.
+"""
+import numpy as np
+import pyopencl
+import scipy.signal
+import time
+
+import pyqtgraph as pg
+
+from pyacq.dsp.sosfilter import  HAVE_PYOPENCL, sosfilter_engines
+
+
+def compare(chunksize,n_section, nb_channel):
+    nloop = 10
+    f1, f2 = 50., 150.
+    sample_rate = 1000.
+    
+
+    dtype = 'float32'
+    data = np.random.randn(nloop*chunksize, nb_channel).astype('float32')
+    coefficients = scipy.signal.iirfilter(n_section, [f1/sample_rate*2, f2/sample_rate*2],
+                btype = 'bandpass', ftype = 'butter', output = 'sos')
+    
+
+    if HAVE_PYOPENCL:
+        engines = ['numpy', 'opencl', 'opencl2']
+    else:
+        engines = ['numpy']
+    
+    times = []
+    for engine in engines:
+        EngineClass = sosfilter_engines[engine]
+        filter_engine = EngineClass(coefficients, nb_channel, dtype, chunksize)
+        
+        t1 = time.perf_counter()
+        for i in range(nloop):
+            chunk = data[i*chunksize:(i+1)*chunksize,:]
+            filter_engine.compute_one_chunk(chunk)
+        t2 = time.perf_counter()
+        
+        times.append(t2-t1)
+    
+    order = np.argsort(times)
+    print([('{} {:.3f}s'.format(engines[i], times[i])) for i in order ])
+    
+        
+
+def benchmark_sosfilter():
+    ap = pg.mkQApp()
+    
+    ctx = pyopencl.create_some_context()
+    print(ctx)
+    
+    #~ chunksizes = [256,1024,2048]
+    chunksizes = [2048]
+    #~ chunksizes = [512]
+    #~ n_sections = [2,8,16,24]
+    n_sections = [8]
+    nb_channels = [1,10, 50,100, 200]
+    #~ chunksizes = [1024]
+    #~ n_sections = [4]
+    #~ nb_channels = [100]
+
+    
+    for chunksize in chunksizes:
+        for n_section in n_sections:
+            for nb_channel in nb_channels:
+                print('*'*20)
+                print('chunksize', chunksize, 'n_section', n_section, 'nb_channel', nb_channel)
+                compare(chunksize,n_section, nb_channel)
+                
+
+    
+    
+    
+    
+if __name__ == '__main__':
+    benchmark_sosfilter()
--- pyacq/dsp/tests/test_sosfilter.py
+++ pyacq/dsp/tests/test_sosfilter.py
@@ -4,7 +4,7 @@ import numpy as np
 import pyqtgraph as pg
 
 from pyacq import create_manager, NumpyDeviceBuffer
-from pyacq.dsp.sosfilter import SosFilter, HAVE_PYOPENCL
+from pyacq.dsp.sosfilter import SosFilter, HAVE_PYOPENCL, sosfilter_engines
 from pyacq.viewers.qoscilloscope import QOscilloscope
 
 from pyqtgraph.Qt import QtCore, QtGui
@@ -13,33 +13,34 @@ import scipy.signal
 
 nb_channel = 4
 sample_rate =1000.
-chunksize = 500
+#~ chunksize = 500
+chunksize = 100
+nloop = 20
 
-length = int(sample_rate*20)
+length = int(chunksize*nloop)
 times = np.arange(length)/sample_rate
-buffer = np.random.rand(nb_channel, length) *.3
+buffer = np.random.rand(length, nb_channel) *.3
 f1, f2, speed = 20., 60., .05
 freqs = (np.sin(np.pi*2*speed*times)+1)/2 * (f2-f1) + f1
 phases = np.cumsum(freqs/sample_rate)*2*np.pi
 ampl = np.abs(np.sin(np.pi*2*speed*8*times))*.8
-buffer += (np.sin(phases)*ampl)[None, :]
+buffer += (np.sin(phases)*ampl)[:, None]
+
 buffer = buffer.astype('float32')
 
 
-
-stream_spec = dict(protocol='tcp', interface='127.0.0.1', transfermode='sharedarray',
-                        sharedarray_shape=(nb_channel, 2048*50), ring_buffer_method = 'double',
-                        dtype = 'float32',)
-                        # timeaxis = 1, shape = (nb_channel, -1),
-
-def do_filtertest(FilterClass, engine):
+def do_filtertest(engine):
+    stream_spec = dict(protocol='tcp', interface='127.0.0.1', transfermode='sharedarray',
+                            sharedarray_shape=(2048*50, nb_channel), ring_buffer_method = 'double',
+                            dtype = 'float32',)
+    
     app = pg.mkQApp()
     
                             
     dev = NumpyDeviceBuffer()
     dev.configure(nb_channel=nb_channel, sample_interval=1./sample_rate, chunksize=chunksize,
-                    buffer=buffer, timeaxis=1,)
-    dev.output.configure(**stream_spec)
+                    buffer=buffer, timeaxis=0,)
+    dev.output.configure( **stream_spec)
     dev.initialize()
     
     
@@ -48,7 +49,7 @@ def do_filtertest(FilterClass, engine):
     coefficients = scipy.signal.iirfilter(7, [f1/sample_rate*2, f2/sample_rate*2],
                 btype = 'bandpass', ftype = 'butter', output = 'sos')
     
-    filter = FilterClass()
+    filter = SosFilter()
     filter.configure(coefficients = coefficients, engine=engine, chunksize=chunksize)
     filter.input.connect(dev.output)
     filter.output.configure(**stream_spec)
@@ -87,74 +88,59 @@ def do_filtertest(FilterClass, engine):
     app.exec_()
 
 def test_sosfilter():
-    do_filtertest(SosFilter, 'numpy')
+    do_filtertest('numpy')
 
 @pytest.mark.skipif(not HAVE_PYOPENCL, reason='no pyopencl')
 def test_openclsosfilter():
-    do_filtertest(SosFilter, 'opencl')
+    do_filtertest('opencl')
+    do_filtertest('opencl2')
 
 
 
-def compare_online_offline(FilterClass, engine):
-
-    #~ man = create_manager(auto_close_at_exit=True)
-    man = create_manager(auto_close_at_exit=False)
-    ng = man.create_nodegroup()
-
-    dev = ng.create_node('NumpyDeviceBuffer')
-    dev.configure(nb_channel=nb_channel, sample_interval=1./sample_rate, chunksize=chunksize,
-                    buffer=buffer, timeaxis=1,)
-    dev.output.configure(**stream_spec)
-    dev.initialize()
-
+def compare_online_offline_engines():
+    
+    if HAVE_PYOPENCL:
+        engines = ['numpy', 'opencl', 'opencl2']
+    else:
+        engines = ['numpy']
+    
+    
+    dtype = 'float32'
+    
     coefficients = scipy.signal.iirfilter(7, [f1/sample_rate*2, f2/sample_rate*2],
                 btype = 'bandpass', ftype = 'butter', output = 'sos')
     
-    filter = ng.create_node(FilterClass.__name__)
-    filter.configure(coefficients = coefficients, engine=engine, chunksize=chunksize)
-    #~ filter.configure(coefficients = coefficients)
-    filter.input.connect(dev.output)
-    filter.output.configure(**stream_spec)
-    filter.initialize()
+    offline_arr =  scipy.signal.sosfilt(coefficients.astype('float32'), buffer.astype('float32'), axis=0, zi=None)
     
-    filter.start()
-    dev.start()
+    for engine in engines:
+        print(engine)
+        EngineClass = sosfilter_engines[engine]
+        filter_engine = EngineClass(coefficients, nb_channel, dtype, chunksize)
+        print(filter_engine)
+        online_arr = np.zeros_like(offline_arr)
+        
+        for i in range(nloop):
+            #~ print(i)
+            chunk = buffer[i*chunksize:(i+1)*chunksize,:]
+            chunk_filtered = filter_engine.compute_one_chunk(chunk)
+            #~ print(chunk_filtered.shape)
+            #~ print(online_arr[i*chunksize:(i+1)*chunksize,:])
+            online_arr[i*chunksize:(i+1)*chunksize,:] = chunk_filtered
+            #~ print(online_arr[i*chunksize:(i+1)*chunksize,:])
+
+        residual = np.abs((online_arr.astype('float64')-offline_arr.astype('float64'))/np.mean(np.abs(offline_arr.astype('float64'))))
+        print(np.max(residual))
+        assert np.max(residual)<5e-5, 'online differt from offline'
     
-    time.sleep(2.)
-    dev.stop()
-    time.sleep(.1)
-    filter.stop()
-    
-    
-    head = dev.head._get_value()
-    output_arr = filter.output.sender._numpyarr._get_value()
-    #~ output_arr = filter.output.sender._numpyarr
-    output_arr = output_arr[:, :head]
-    
-    
-    offline_arr =  scipy.signal.sosfilt(coefficients.astype('float32'), buffer[:, :head].astype('float32'), axis=1, zi=None)
-    
-    residual = np.abs((output_arr.astype('float64')-offline_arr.astype('float64'))/np.mean(np.abs(offline_arr.astype('float64'))))
-    assert np.max(residual)<5e-5, 'online differt from offline'
-    
-    #~ from matplotlib import pyplot
-    #~ fig, ax = pyplot.subplots()
-    #~ ax.plot(output_arr[0,:], color = 'r')
-    #~ ax.plot(offline_arr[0,:], color = 'g')
-    #~ fig, ax = pyplot.subplots()
-    #~ for c in range(nb_channel):
-        #~ ax.plot(residual[c,:], color = 'k')
+        #~ from matplotlib import pyplot
+        #~ fig, ax = pyplot.subplots()
+        #~ ax.plot(online_arr[:, 2], color = 'r')
+        #~ ax.plot(offline_arr[:, 2], color = 'g')
+        #~ fig, ax = pyplot.subplots()
+        #~ for c in range(nb_channel):
+            #~ ax.plot(residual[:, c], color = 'k')
     #~ pyplot.show()
-    
-    man.close()
-
-def test_compare_sosfilter():
-    compare_online_offline(SosFilter, 'numpy')
-
-@pytest.mark.skipif(not HAVE_PYOPENCL, reason='no pyopencl')
-def test_compare_openclsosfilter():
-    compare_online_offline(SosFilter, 'opencl')
-
+        
     
     
     
@@ -162,7 +148,7 @@ def test_compare_openclsosfilter():
 if __name__ == '__main__':
     test_sosfilter()
     test_openclsosfilter()
-    test_compare_sosfilter()
-    test_compare_openclsosfilter()
+    
+    compare_online_offline_engines()
 
  
