d1bf336606719bb2fb58a96decef03352f72b4f4
Samuel Garcia <sam/garcia.die@gmail.com>
2016-09-05 09:40:33 +0200

    RingBuffer in init

--- pyacq/core/__init__.py
+++ pyacq/core/__init__.py
@@ -4,5 +4,5 @@ from .nodegroup import NodeGroup
 from .node import Node, WidgetNode
 from .nodelist import register_node_type
 from .manager import Manager, create_manager
-from .stream import OutputStream, InputStream, SharedArray
+from .stream import OutputStream, InputStream, SharedArray, RingBuffer
 from .tools import ThreadPollInput, ThreadPollOutput, StreamConverter, ChannelSplitter
