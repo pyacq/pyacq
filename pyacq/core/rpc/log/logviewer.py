2747eb4a95231e3b2c09aee8f4d403563bafdd8f
Luke Campagnola <luke.campagnola@gmail.com>
2017-06-06 09:04:08 -0700

    Add ThreadSpawner, fix LogViewer

--- pyacq/core/rpc/log/logviewer.py
+++ pyacq/core/rpc/log/logviewer.py
@@ -4,6 +4,7 @@
 
 import logging
 from pyqtgraph.Qt import QtCore, QtGui
+from .remote import get_host_name, get_process_name, get_thread_name
 
 
 Stylesheet = """
@@ -18,10 +19,21 @@ Stylesheet = """
     .timestamp {color: #000;}
 """
 
+_thread_color_list = [
+    "#AA0000",
+    "#00AA00",
+    "#0000AA",
+    "#888800",
+    "#880088",
+    "#008888",
+]
+
 
 class LogViewer(QtGui.QWidget):
     """QWidget for displaying and filtering log messages.
     """
+    thread_headers = {}
+
     def __init__(self, logger='', parent=None):
         QtGui.QWidget.__init__(self, parent=parent)
         
@@ -38,10 +50,27 @@ class LogViewer(QtGui.QWidget):
         self.setLayout(self.layout)
         self.text = QtGui.QTextBrowser()
         self.text.document().setDefaultStyleSheet(Stylesheet)
+        self.layout.addWidget(self.text, 0, 0)
+        
+        self.resize(800, 600)
         
     def new_record(self, rec):
-        print("NEW LOG RECORD:", rec)
+        self.last_rec = rec
+        header = self.get_thread_header(rec)
+        self.text.append("%s %s\n" % (header, rec.getMessage()))
         
+    def get_thread_header(self, record):
+        hid = getattr(record, 'hostname', get_host_name())
+        pid = getattr(record, 'process_name', get_process_name())
+        tid = getattr(record, 'thread_name', get_thread_name(record.thread))
+        key = (hid, pid, tid)
+        header = self.thread_headers.get(key, None)
+        if header is None:
+            header = '[%s:%s:%s]' % (hid, pid, tid)
+            color = _thread_color_list[len(self.thread_headers) % len(_thread_color_list)]
+            header = '<span style="color: %s;">%s</span>' % (color, header)
+            self.thread_headers[key] = header
+        return header
         
 
 class QtLogHandler(logging.Handler, QtCore.QObject):
@@ -54,4 +83,4 @@ class QtLogHandler(logging.Handler, QtCore.QObject):
         QtCore.QObject.__init__(self)
         
     def handle(self, record):
-        new_record.emit(record)
+        self.new_record.emit(record)
