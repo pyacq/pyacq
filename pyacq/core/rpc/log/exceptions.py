c5a079597a19096bae0afe378537100134b6ab0e
Luke Campagnola <luke.campagnola@gmail.com>
2017-07-06 08:34:44 -0700

    Better formatting for exception strings

--- pyacq/core/rpc/log/exceptions.py
+++ pyacq/core/rpc/log/exceptions.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import traceback
 
 _exception_logger = None
 
@@ -13,6 +14,7 @@ class ExceptionLogger(object):
 
         self.log_level = log_level
         self.call_orig_hook = call_orig_hook
+        self.orig_hook = None
         
     def install(self):
         if sys.excepthook is self.log_exception:
@@ -29,16 +31,17 @@ class ExceptionLogger(object):
     def log_exception(self, exc, val, tb):
         logger = self.logger
         if logger is not None:
-            extra = {'exc_info': (exc, val, tb)}
-            logger.log(self.log_level, "Unhandled exception: %s", str(exc), extra=extra)
-        if self.call_orig_hook:
+            msg = ''.join(traceback.format_exception_only(exc, val)).rstrip()
+            #extra = {'exc_info': (exc, val, tb)}
+            logger.log(self.log_level, "(unhandled) %s", msg)#, extra=extra)
+        if self.call_orig_hook and self.orig_hook is not None:
             self.orig_hook(exc, val, tb)
 
 
-def log_exceptions(logger=None, log_level=logging.WARN, call_orig_hook=False):
+def log_exceptions(logger=None, log_level=logging.WARN, call_orig_hook=True):
     """Install a hook that creates log messages from unhandled exceptions.
     """
     global _exception_logger
     if _exception_logger is None:
-        _exception_logger = ExceptionLogger(logger, call_orig_hook)
+        _exception_logger = ExceptionLogger(logger, log_level=log_level, call_orig_hook=call_orig_hook)
     _exception_logger.install()
